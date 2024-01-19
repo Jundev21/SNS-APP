f3b62317b937d1ebe496425c826f6d952a41be6c
jun <jundev21@gmail.com>
2024-01-20 01:09:04 +0900

    [feat] 1차 템플릿 완료

--- src/main/java/com/sns/sns/service/domain/comment/dto/request/CommentPostRequest.java
+++
@@ -1,6 +0,0 @@
-package com.sns.sns.service.domain.comment.dto.request;
-
-public record CommentPostRequest (
-        String content
-){
-}
--- src/main/java/com/sns/sns/service/domain/member/dto/request/LoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.sns.sns.service.domain.member.dto.request;
-
-public record LoginRequest(
-        String userName,
-        String password
-) {
-
-}
