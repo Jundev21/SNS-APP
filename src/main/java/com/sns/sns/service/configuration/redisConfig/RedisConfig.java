efa7d9a7f1ae1de5cd8c64ce7a2e8a28fe6f4b65
jun <jundev21@gmail.com>
2024-02-03 23:13:05 +0900

    [update] redis 세팅

--- src/main/java/com/sns/sns/service/configuration/redisConfig/RedisConfig.java
+++ src/main/java/com/sns/sns/service/configuration/redisConfig/RedisConfig.java
@@ -4,6 +4,7 @@ package com.sns.sns.service.configuration.redisConfig;
 import com.sns.sns.service.domain.member.model.entity.Member;
 import io.lettuce.core.RedisURI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,29 +16,48 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-//@Configuration
-//@EnableRedisRepositories
+@Configuration
+@EnableRedisRepositories
 @RequiredArgsConstructor
 public class RedisConfig {
 
     // redis url 데이터 설정
     private final RedisProperties redisProperties;
 
+//    @Value("${spring.data.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.data.redis.port}")
+//    private int port;
+
+
+    //redisConnectionFactory 는 어떤 레디스 서버랑 통신해야하는지 설정하는 곳
+    //
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory(){
+//        RedisURI redisURI = RedisURI.create(redisProperties.getUrl());
+//        RedisConfiguration configuration = LettuceConnectionFactory.createRedisConfiguration(redisURI);
+//        LettuceConnectionFactory factory = new LettuceConnectionFactory(configuration);
+//        factory.afterPropertiesSet();
+//        return factory;
+//    }
+
+
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
-        RedisURI redisURI = RedisURI.create(redisProperties.getUrl());
-        RedisConfiguration configuration = LettuceConnectionFactory.createRedisConfiguration(redisURI);
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(configuration);
-        factory.afterPropertiesSet();
-        return factory;
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisProperties.getHost(),redisProperties.getPort());
+        // 패스워드가 있는경우
+        // lettuceConnectionFactory.setPassword("");
+        return lettuceConnectionFactory;
     }
 
     // RedisTemplate 는 redis 데이터를 접근할수 있도록 도와주는 클래스
     // Key,value 타입
+    // 사용자에 대한 정보를 캐싱하기위한 클래스
     @Bean
     public RedisTemplate<String, Member> memberRedisTemplate(RedisConnectionFactory redisConnectionFactory){
         RedisTemplate<String, Member> redisTemplate = new RedisTemplate<>();
-
         //redis 서로 연결
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
